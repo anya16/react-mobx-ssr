ca4b555daa75bd9ca8e31127ee6172a61a603fcf
anya16 <1325854867@qq.com>
2018-10-28 16:14:03 +0800

    add eslint and editorconfig

--- client/App.jsx
+++ client/App.jsx
@@ -1,9 +1,3 @@
-import React,{Component} from 'react'
+import React from 'react'
 
-export default class App extends Component{
-    render() {
-        return (
-            <div>this is app123</div>
-        )
-    }
-}
+export default () => <div>this is app123</div>
